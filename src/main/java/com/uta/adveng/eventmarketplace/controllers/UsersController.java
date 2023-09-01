8896c79c89bbdaffed64311dd4461e899caf550e
Erandi Ranthilake <erandiranthilake@gmail.com>
2023-08-31 21:56:48 -0500

    controller renamed

--- src/main/java/com/uta/adveng/eventmarketplace/controllers/UsersController.java
+++ src/main/java/com/uta/adveng/eventmarketplace/controllers/UsersController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UsersRestController {
+public class UsersController {
 
     @GetMapping("/users/getUsers")
     public String getUsers() {
